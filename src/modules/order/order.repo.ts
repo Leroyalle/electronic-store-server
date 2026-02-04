c987361beec773274aad0d7ccf7e0d7abcbfb22a
Leroyalle <nikolajmelonov@yandex.ru>
2026-02-04 14:37:16 +0300

    feat(order): add repo

--- src/modules/order/order.repo.ts
+++ src/modules/order/order.repo.ts
@@ -1,5 +1,17 @@
-import { Order } from '@/shared/db/schema/order.schema';
+import { db } from '@/shared/db/client';
+import { Order, orderSchema } from '@/shared/db/schema/order.schema';
 
 export interface IOrderRepository {
   create(data: Omit<Order, 'id'>): Promise<Order>;
+  findAll(): Promise<Order[]>;
+}
+
+export class OrderRepo implements IOrderRepository {
+  public async create(data: Omit<Order, 'id'>): Promise<Order> {
+    return (await db.insert(orderSchema).values(data).returning())[0];
+  }
+
+  public async findAll(): Promise<Order[]> {
+    return await db.query.orderSchema.findMany();
+  }
 }
