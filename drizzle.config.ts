f6dc1e4305abb8c99f1e5170bd36e9cefb4d11ee
Leroyalle <nikolajmelonov@yandex.ru>
2026-02-04 17:56:55 +0300

    refactor(drizzle-config): get database url via env helper

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,13 @@
 import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
+import { getEnv } from '@/shared/lib/helpers/get-env.helper';
+
 export default defineConfig({
   out: './drizzle',
   schema: './src/shared/db/schema/*.ts',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: getEnv('DATABASE_URL'),
   },
 });
