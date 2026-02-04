b0190f9f306e645fec52af20ddbcf8bff07f0de6
Leroyalle <nikolajmelonov@yandex.ru>
2026-02-04 09:53:46 +0300

    feat(product): add router

---
+++ src/modules/product/product.router.ts
@@ -0,0 +1,33 @@
+import { Hono } from 'hono';
+
+import { ProductCommands } from './product.commands';
+import { ProductQueries } from './product.queries';
+
+interface Deps {
+  commands: ProductCommands;
+  queries: ProductQueries;
+}
+
+export function createProductRouter(deps: Deps): Hono {
+  const productRouter = new Hono();
+
+  productRouter.get('/', c => {
+    const data = deps.queries.findAll();
+    return c.json(data);
+  });
+
+  // TODO: гвард на авторизацию админа
+  productRouter.post('/', async c => {
+    const body = await c.req.json<{ name: string; price: number }>();
+    const data = await deps.commands.create(body);
+    return c.json(data, 201);
+  });
+
+  productRouter.get('/:id', c => {
+    const id = c.req.param('id');
+    const data = deps.queries.findById(id);
+    return c.json(data);
+  });
+
+  return productRouter;
+}
