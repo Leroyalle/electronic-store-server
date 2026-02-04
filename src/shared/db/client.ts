8047b05b820f9cfdfb40397dc3b9258721de6892
Leroyalle <nikolajmelonov@yandex.ru>
2026-02-04 17:55:18 +0300

    refactor(client): extract get env into shared helpers

--- src/shared/db/client.ts
+++ src/shared/db/client.ts
@@ -1,5 +1,7 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 
+import { getEnv } from '../lib/helpers/get-env.helper';
+
 import * as cartItemSchema from './schema/cart-item.schema';
 import * as cartSchema from './schema/cart.schema';
 import * as orderSchema from './schema/order.schema';
@@ -7,15 +9,7 @@ import * as productSchema from './schema/product.schema';
 import * as refreshTokenSchema from './schema/refresh-token.schema';
 import * as userSchema from './schema/user.schema';
 
-function getEnv(key: string) {
-  const value = process.env[key];
-  if (!value) {
-    throw new Error(`Missing environment variable: ${key}`);
-  }
-  return value;
-}
-
-export const db = drizzle(getEnv(process.env.DATABASE_URL!), {
+export const db = drizzle(getEnv('DATABASE_URL'), {
   schema: {
     ...cartSchema,
     ...cartItemSchema,
