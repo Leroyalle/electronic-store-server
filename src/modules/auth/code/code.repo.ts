2f64d6132e637aedaee848d526594875e5f1da73
Leroyalle <nikolajmelonov@yandex.ru>
2026-02-09 02:37:39 +0300

    feat(code): improve find by user id with find by code and type

--- src/modules/auth/code/code.repo.ts
+++ src/modules/auth/code/code.repo.ts
@@ -1,11 +1,11 @@
-import { eq } from 'drizzle-orm';
+import { and, eq } from 'drizzle-orm';
 
 import { db } from '@/shared/infrastructure/db/client';
 import { AuthCode, authCodeSchema } from '@/shared/infrastructure/db/schema/auth-code.schema';
 
 export interface ICodeRepository {
   create(data: Pick<AuthCode, 'code' | 'userId' | 'type'>): Promise<AuthCode>;
-  findByUserId(userId: string): Promise<AuthCode | undefined>;
+  findByUserId(data: Pick<AuthCode, 'code' | 'userId' | 'type'>): Promise<AuthCode | undefined>;
 }
 
 export class CodeRepo implements ICodeRepository {
@@ -13,7 +13,15 @@ export class CodeRepo implements ICodeRepository {
     return (await db.insert(authCodeSchema).values(data).returning())[0];
   }
 
-  public async findByUserId(userId: string): Promise<AuthCode | undefined> {
-    return await db.query.authCodeSchema.findFirst({ where: eq(authCodeSchema.userId, userId) });
+  public async findByUserId(
+    data: Pick<AuthCode, 'code' | 'userId' | 'type'>,
+  ): Promise<AuthCode | undefined> {
+    return await db.query.authCodeSchema.findFirst({
+      where: and(
+        eq(authCodeSchema.userId, data.userId),
+        eq(authCodeSchema.type, data.type),
+        eq(authCodeSchema.code, data.code),
+      ),
+    });
   }
 }
