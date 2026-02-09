ae1541b52af88e9e55b9a4e48bc3ff0ff7565ad0
Leroyalle <nikolajmelonov@yandex.ru>
2026-02-09 11:53:47 +0300

    refactor(queue): add generic for result and name

--- src/shared/infrastructure/bullmq/queue-factory.ts
+++ src/shared/infrastructure/bullmq/queue-factory.ts
@@ -1,11 +1,11 @@
 import { ConnectionOptions, Queue, QueueOptions } from 'bullmq';
 
-export function createQueue<T = any>(
+export function createQueue<T = any, R = any, N extends string = string>(
   queueName: string,
   connection: ConnectionOptions,
   opts?: Omit<QueueOptions, 'connection'>,
 ) {
-  return new Queue<T>(queueName, {
+  return new Queue<T, R, N>(queueName, {
     connection,
     ...opts,
     defaultJobOptions: {
