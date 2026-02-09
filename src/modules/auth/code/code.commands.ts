e16cc078af26788b330d40e6790b859d66e56fd8
Leroyalle <nikolajmelonov@yandex.ru>
2026-02-09 12:36:13 +0300

    feat(code): save code to redis

--- src/modules/auth/code/code.commands.ts
+++ src/modules/auth/code/code.commands.ts
@@ -1,14 +1,17 @@
+import Redis from 'ioredis';
+import crypto from 'node:crypto';
+
 import { AuthCode } from '@/shared/infrastructure/db/schema/auth-code.schema';
 
-import { ICodeRepository } from './code.repo';
-
 interface Deps {
-  codeRepo: ICodeRepository;
+  redis: Redis;
 }
 
 export class CodeCommands {
   constructor(private readonly deps: Deps) {}
-  public create(data: Pick<AuthCode, 'code' | 'userId' | 'type'>) {
-    return this.deps.codeRepo.create(data);
+  public async create(data: Pick<AuthCode, 'userId' | 'type'>) {
+    const code = crypto.randomInt(1000, 10000);
+    await this.deps.redis.set(`auth:code:${data.type}:${data.userId}`, code, 'EX', 60);
+    return code;
   }
 }
