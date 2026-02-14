ce1d15715575c887c7e7cf60ae6215041f839559
Leroyalle <nikolajmelonov@yandex.ru>
2026-02-14 19:56:26 +0300

    feat(account): add update method

--- src/modules/auth/account/account.repo.ts
+++ src/modules/auth/account/account.repo.ts
@@ -8,11 +8,12 @@ import {
   credentialsAccountSchema,
   oauthAccountSchema,
 } from '@/shared/infrastructure/db/schema/account.schema';
-import { ICreateAccount } from '@/shared/types/auth/create-account.type';
+import { ICreateAccount, IUpdateAccount } from '@/shared/types/auth/create-account.type';
 
 export interface IAccountRepository {
   findById(id: string): Promise<AccountWithRelations | undefined>;
   create(data: Omit<ICreateAccount, 'id' | 'createdAt' | 'updatedAt'>): Promise<Account>;
+  update(id: string, data: IUpdateAccount): Promise<Account>;
 }
 
 export class AccountRepo implements IAccountRepository {
@@ -26,11 +27,33 @@ export class AccountRepo implements IAccountRepository {
     });
   }
 
+  public async update(id: string, data: IUpdateAccount): Promise<Account> {
+    const [updatedAccount] = await db
+      .update(accountSchema)
+      .set(data.account)
+      .where(eq(accountSchema.id, id))
+      .returning();
+
+    if (data.providerDetails.type === 'oauth') {
+      await db
+        .update(oauthAccountSchema)
+        .set(data.providerDetails)
+        .where(eq(oauthAccountSchema.accountId, updatedAccount.id));
+    } else if (data.providerDetails.type === 'credentials') {
+      await db
+        .update(credentialsAccountSchema)
+        .set(data.providerDetails)
+        .where(eq(credentialsAccountSchema.accountId, updatedAccount.id));
+    }
+
+    return updatedAccount;
+  }
+
   public async create(
     data: Omit<ICreateAccount, 'id' | 'createdAt' | 'updatedAt'>,
   ): Promise<Account> {
     return db.transaction(async tx => {
-      const account = (await tx.insert(accountSchema).values(data.account).returning())[0];
+      const [account] = await tx.insert(accountSchema).values(data.account).returning();
 
       if (data.providerDetails.type === 'oauth') {
         await tx
@@ -45,7 +68,6 @@ export class AccountRepo implements IAccountRepository {
           .insert(credentialsAccountSchema)
           .values({
             accountId: account.id,
-            email: data.providerDetails.email,
             password: data.providerDetails.password,
           })
           .returning();
